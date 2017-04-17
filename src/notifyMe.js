f0514d085e6a67e885afc73ba3c330ec6f4a18c9
Parth <ppmistry204@gmail.com>
2017-04-18 00:29:55 +0530

    removed demoCtrl, added CDN for angular libs.

--- src/notifyMe.js
+++ src/notifyMe.js
@@ -2,11 +2,4 @@
  * Created by parth on 15/04/17.
  */
 
-angular.module('notifyMe', ['ngAnimate'])
-    .controller('demoCtrl', ['notifyMe', function(NotifyMeService) {
-        this.btnText = 'Click to see notification.';
-
-        this.notifyError = function() {
-            NotifyMeService.info('Hey bro, new error', 'Welcome');
-        };
-    }]);
+angular.module('notifyMe', ['ngAnimate']);
